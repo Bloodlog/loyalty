ef859146501e22eac607681387ffe8bd8da10d5f
Maksim Fomin <bloodlog@gmail.com>
2025-02-22 20:47:46 +0400

    Исправление теста

--- internal/app/utils/utils_test.go
+++ internal/app/utils/utils_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestLuhnCheck(t *testing.T) {
-	assert.True(t, LuhnCheck(1234567890123456))
-	assert.True(t, LuhnCheck(1234567812345678901))
+	assert.True(t, LuhnCheck(2377225624))
+	assert.True(t, LuhnCheck(24141463521))
 
 	assert.False(t, LuhnCheck(12345))
 }
